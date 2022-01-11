bc472913cdeaf698893a3450c79eb135d07ff8c1
Tinghan Shen <tinghan.shen@mediatek.com>
2022-01-11 17:18:04 +0800

    scripts: Add mt8186 to build script
    Add mt8186 to build script
    
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>
    Signed-off-by: Allen-KH Cheng <allen-kh.cheng@mediatek.com>

--- scripts/xtensa-build-all.sh
+++ scripts/xtensa-build-all.sh
@@ -6,7 +6,7 @@
 set -e
 
 SUPPORTED_PLATFORMS=(byt cht bdw hsw apl skl kbl cnl sue icl jsl \
-                    imx8 imx8x imx8m imx8ulp tgl tgl-h rn mt8195)
+                    imx8 imx8x imx8m imx8ulp tgl tgl-h rn mt8186 mt8195)
 BUILD_ROM=no
 BUILD_DEBUG=no
 BUILD_FORCE_UP=no
@@ -329,6 +329,12 @@ do
 			HOST="xtensa-rn-elf"
 			XTENSA_TOOLS_VERSION="RF-2016.4-linux"
 			;;
+                mt8186)
+                        PLATFORM="mt8186"
+                        XTENSA_CORE="hifi5_7stg_I64D128"
+                        HOST="xtensa-mt8186-elf"
+                        XTENSA_TOOLS_VERSION="RI-2020.5-linux"
+                        ;;
 		mt8195)
 			PLATFORM="mt8195"
 			XTENSA_CORE="hifi4_8195_PROD"
